f62da99327dc61214cefc475872f7de417c72a3a
Nazmus <nsakib6685@bths.edu>
2018-09-19 19:05:40 -0400

    Completed:
     isFibonacci
     sumUpTo
     multiplicationTable
     quadSolver

--- src/NSakib/NSakibLib.java
+++ src/NSakib/NSakibLib.java
@@ -34,13 +34,69 @@ public class NSakibLib {
         if (i == 0)
         {
             finalStr = mainStr.substring(subStr.length());
-        } else
-        {
-            finalStr = mainStr.substring(0,i) + mainStr.substring(i+subStr.length());
+        } else {
+            finalStr = mainStr.substring(0, i) + mainStr.substring(i + subStr.length());
         }
         System.out.println(finalStr);
     }
 
+    public static void isFibonacci(int num)
+    {
+        int fibonacciNum = 1;
+        int previousNum = 0;
+        int placeHolder = 0;
+        while (fibonacciNum < num)
+        {
+            placeHolder = fibonacciNum;
+            fibonacciNum += previousNum;
+            previousNum =  placeHolder;
+        }
+        if (fibonacciNum == num )
+        {
+            printLn("True");
+        } else {
+            printLn("False");
+        }
+    }
+
+    public static void sumUpTo(int num)
+    {
+        int finalSum= 0;
+        for (int i=0; i <= num; i++)
+        {
+            finalSum+=i;
+        }
+        printLn(String.valueOf(finalSum));
+    }
+
+    public static void multiplicationTable(int base,int range)
+    {
+        String table = "";
+        for (int i = 0; i <= range; i++)
+        {
+            table += String.valueOf(base)+"*"+String.valueOf(i)+"="+String.valueOf(base*i)+"; ";
+        }
+        printLn(table);
+    }
+
+    public static void quadSolver(double a, double b,double c)
+    {
+        double root1 = 0;
+        double root2 = 0;
+        double discriminant = (b*b)-4*a*c;
+        if ( discriminant < 0)
+        {
+            printLn("");
+            printLn( String.valueOf(a)+"x^2 + "+String.valueOf(b)+"x + "+String.valueOf(c)+" " + " Has No Real Roots");
+        } else
+        {
+            root1 = (-b + Math.sqrt(Math.pow(b,2)-4*a*c))/(2*a);
+            root2 = (-b - Math.sqrt(Math.pow(b,2)-4*a*c))/(2*a);
+            printLn("");
+            printLn(String.valueOf(a)+"x^2 + "+String.valueOf(b)+"x + "+String.valueOf(c)+" ");
+            printLn("Root = "+String.valueOf(root1) +"  Root = "+String.valueOf(root2));
+        }
+    }
 
 
 }
--- src/NSakib/TestFile.java
+++ src/NSakib/TestFile.java
@@ -1,18 +1,32 @@
 package NSakib;
 
 
-import static NSakib.NSakibLib.cutOut;
-import static NSakib.NSakibLib.isPalindrome;
-import static NSakib.NSakibLib.printLn;
+import static NSakib.NSakibLib.*;
 
 public class TestFile {
 
     public static void main(String[] args)
     {
         printLn("I love a cup of Java in the morning.");
+
+        // String Methods
+
         isPalindrome("racecar");
         isPalindrome("nope");
-        cutOut("ashellofg","hell");
+
+        cutOut("ashellofg","hello");
+
+        // Math Methods
+        isFibonacci(10946);
+        isFibonacci(0);
+
+        sumUpTo(15);
+
+        multiplicationTable(4,10);
+
+        // Challenge Methods
+        quadSolver(1,4,4);
+        quadSolver(1,2,4);
     }
 
 
